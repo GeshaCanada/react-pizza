d26b7d77a2839db580de468c9ab9b2c1a40a1594
Gennadii Rostikov <107649327+GeshaCanada@users.noreply.github.com>
2022-12-21 19:57:23 -0500

    Create Components, (props), SCSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-function App() {
+import React from 'react'
 
+import Categories from './components/Categories'
+import Header from './components/Header'
+import PizzaBlock from './components/PizzaBlock'
+import Sort from './components/Sort'
+import './scss/app.scss'
+
+
+function App() {
   return (
-    <div className="App">
-      <h1>Hello,world!</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">All pizzas</h2>
+          <div class="content__items">
+            <PizzaBlock title = 'Mexican' price = 'only 9.99 CAD'/>
+            <PizzaBlock title = 'Italian' price ='only 10.99 CAD' />
+            <PizzaBlock title = 'Ukranian'price ='Coming soon!'/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat</li>
+        <li>Vegetable</li>
+        <li>Grilled</li>
+        <li>Spicy</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Categories
