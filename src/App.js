c6d60128d370c9173bca8b50cb76a0fc1270e24e
Gennadii Rostikov <107649327+GeshaCanada@users.noreply.github.com>
2023-01-02 19:09:57 -0500

    Sceleton

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from 'react'
 import Categories from './components/Categories'
 import Header from './components/Header'
 import PizzaBlock from './components/PizzaBlock'
+import Sceleton from './components/PizzaBlock/Sceleton'
 import Sort from './components/Sort'
 import './scss/app.scss'
 
 
-
 function App() {
   const [items, setItems] = useState([])
+  const [isLoading, setIsloading] = useState(true)
 
   useEffect(() => {
     fetch('https://63addb98ceaabafcf16e07a0.mockapi.io/items')
       .then((res) => res.json())
       .then((arr) => {
-        setItems(arr)
+        setItems(arr) //render pizza
+        setIsloading(false) // loading done
       })
   }, [])
-  
+
   return (
     <div className="wrapper">
       <Header />
@@ -29,9 +31,10 @@ function App() {
           </div>
           <h2 className="content__title">All pizzas</h2>
           <div className="content__items">
-            {items.map((obj) => (                   // list rendering
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)        // fake Array for rendering Sceleton
+              : items.map((obj) =>
+                <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
